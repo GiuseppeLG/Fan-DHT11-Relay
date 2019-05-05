c30d00fff046a29de5ec1b99d6e96ed0e73cf7e6
GiuseppeLG <info.giuseppelagualano@gmail.com>
2019-05-05 15:32:38 +0200

    Initial commit

---
+++ Fan-DHT11-Relay.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal.h>
+#include <dht.h>
+#define rele1 13
+int sensorPin = A0;
+dht sensor;
+
+void setup() {
+
+pinMode(rele1, OUTPUT); 
+//Serial.begin(9600); //set baud rate
+
+}
+
+void loop() {
+ 
+  sensor.read11(sensorPin);
+  //String myString = String(sensor.temperature);
+  //String myString2 = String(sensor.humidity);
+
+  if ((sensor.temperature) >= 24.00){
+    digitalWrite(rele1, HIGH);
+  }
+  else if (((sensor.temperature) >= 23.00) && ((sensor.humidity) >= 55.00)){
+    digitalWrite(rele1, HIGH);
+  }
+  else if (((sensor.temperature) >= 22.00) && ((sensor.humidity) >= 60.00)){
+    digitalWrite(rele1, HIGH);
+  }
+  else {
+    digitalWrite(rele1, LOW);
+  }
+  
+  //Serial.println(myString);
+  //Serial.println(myString2);
+
+delay(10000); //10 sec
+}
