f3a60df716ccd42b42205a0a5d0eeff5534d0fea
Giuseppe La Gualano <info.giuseppelagualano@gmail.com>
2021-06-28 17:05:35 +0200

    Update sensor's values

--- Fan-DHT11-Relay.ino
+++ Fan-DHT11-Relay.ino
@@ -5,30 +5,23 @@ int sensorPin = A0;
 dht sensor;
 
 void setup() {
-
-pinMode(rele1, OUTPUT); 
-//Serial.begin(9600); //set baud rate
-
+  pinMode(rele1, OUTPUT); 
 }
 
 void loop() {
  
   sensor.read11(sensorPin);
-  //String myString = String(sensor.temperature);
-  //String myString2 = String(sensor.humidity);
 
-  if ((sensor.temperature) >= 25.00){
+  if ((sensor.temperature) >= 29.00){
     digitalWrite(rele1, HIGH);
   }
-  else if (((sensor.temperature) >= 24.00) && ((sensor.humidity) >= 60.00)){
+  else if (((sensor.temperature) >= 28.00) && ((sensor.humidity) >= 50.00)){
     digitalWrite(rele1, HIGH);
   }
   else {
     digitalWrite(rele1, LOW);
   }
   
-  //Serial.println(myString);
-  //Serial.println(myString2);
 
 delay(10000); //10 sec
 }
